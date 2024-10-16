f5fb220de0d2fc7180f25a3e62d355c00a11d87a
Aryasatya Wiryawan <aryasatyapark@gmail.com>
2024-10-16 23:06:55 +0700

    Add admin dashboard

--- pharmacommerce/resources/views/layout/app.blade.php
+++ pharmacommerce/resources/views/layout/app.blade.php
@@ -50,8 +50,7 @@
 
               <div class="d-flex justify-content-center py-4">
                 <a href="{{ url('/') }}" class="logo d-flex align-items-center w-auto">
-                  <img src="{{ asset('img/favicon.png') }}" alt="">
-                  <span class="d-none d-lg-block">NiceAdmin</span>
+                  <img src="{{ asset('img/favicon.png') }}" alt=""><span class="d-none d-lg-block">PharmaC</span>
                 </a>
               </div><!-- End Logo -->
 
--- pharmacommerce/routes/web.php
+++ pharmacommerce/routes/web.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', [AuthController::class, 'login']);
 
 Route::get('forgot', [AuthController::class, 'forgot']);
+
+Route::get('admin/dashboard', [DashboardController::class, 'dashboard']);
