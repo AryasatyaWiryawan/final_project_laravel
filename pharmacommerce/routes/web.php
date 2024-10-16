d00053413ff34cff8d40fd44ce9222ceb125ad1d
Aryasatya Wiryawan <aryasatyapark@gmail.com>
2024-10-16 22:29:08 +0700

    Add Login Page & Forgot Password Page

--- pharmacommerce/routes/web.php
+++ pharmacommerce/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AuthController::class, 'login']);
+
+Route::get('forgot', [AuthController::class, 'forgot']);
